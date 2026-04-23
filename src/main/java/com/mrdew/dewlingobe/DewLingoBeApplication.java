5c458cdfbf8053f37fabc0dfa98afadd81769f58
MrCYlmz <celal63159@gmail.com>
2026-04-23 22:29:52 +0200

    initialize Spring Boot application with PostgreSQL setup and Maven wrapper

---
+++ src/main/java/com/mrdew/dewlingobe/DewLingoBeApplication.java
@@ -0,0 +1,13 @@
+package com.mrdew.dewlingobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DewLingoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DewLingoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrdew/dewlingobe/DewLingoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdew.dewlingobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DewLingoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
