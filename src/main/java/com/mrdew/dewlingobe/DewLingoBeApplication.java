5d556b6752393d33f3807be665c41b8a2820fdfc
MrCYlmz <celal63159@gmail.com>
2026-04-23 22:56:52 +0200

    wip

--- src/main/java/com/mrdew/dewlingobe/DewLingoBeApplication.java
+++ src/main/java/com/mrdew/dewlingobe/DewLingoBeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DewLingoBeApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(DewLingoBeApplication.class, args);
     }
